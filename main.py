7645c16e855e0eb9a7bfcad3acb57f81c1caca68
kel <tolentino.kg@obmontessori.edu.ph>
2026-04-17 07:53:19 +0800

    Update plot data and titles in sample_numpy function

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 def sample_numpy(e):
     document.getElementById('output').innerHTML = " " 
 
-    boxes = np.array(['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'])
-    sold_boxes = np.array([4, 2, 3, 4, 7, 3, 1])
+    cases = np.array(['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'])
+    solved_cases = np.array([5, 4, 6, 3, 7, 2, 1])
 
-    plt.plot(boxes, sold_boxes)
+    plt.plot(cases, solved_cases)
 
-    plt.title("Donuts Sold")
+    plt.title("Cases Solved")
     plt.xlabel("Days")
-    plt.ylabel("Number of Boxes")
+    plt.ylabel("Number of Cases ")
 
     plt.show()
